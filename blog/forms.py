a681aa155a1d7d5c4f9977445a0575dfcf0f2682
anastasiya.vynogradska@hotmail.com <anastasiya.vynogradska@hotmail.com>
2015-11-21 17:36:19 +0200

    'd'

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'blog_text'}),
+
+        }
